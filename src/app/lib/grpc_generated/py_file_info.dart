0bb091abd85fa22452663934670480ec3b31097d
Quentin Clément <91249878+Quentin-Clement@users.noreply.github.com>
2025-02-04 14:17:45 +0100

    Revert "flutter x python set up"
    This reverts commit b432c8ea321f77d7660fbbb62397faed00fa1fdf.

--- src/app/lib/grpc_generated/py_file_info.dart
+++
@@ -1,4 +0,0 @@
-// !Will be rewriten upon `prepare sources` or `build` actions by Flutter-Python starter kit
-const versionFileName = 'server_py_version.txt';
-const exeFileName = 'server_py_flutter';
-const currentFileVersionFromAssets = '2025_01_27_15_52_29';
